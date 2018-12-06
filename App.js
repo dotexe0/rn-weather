720886331ba4b873ec3d8bba87ddbed7f38bc740
Daniel Varela <dvarela@wiser.com>
2018-12-06 09:07:00 -0800

    add ImageBackground and assets with utils

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text } from 'react-native';
+import { ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text } from 'react-native';
+
+import getImageForWeather from './utils/getImageForWeather'
+
 import SearchInput from './components/SearchInput'
 
 export default class App extends React.Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <ImageBackground
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        />
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
@@ -19,8 +27,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+  },
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover'
   },
   textStyle: {
     textAlign: 'center',
