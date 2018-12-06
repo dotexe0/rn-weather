eb1c3fe890428883d55243f4228fee137b3a9657
Daniel Varela <dvarela@wiser.com>
2018-12-06 09:31:17 -0800

    wrap Text in a View

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text } from 'react-native'
+import { ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native'
 
 import getImageForWeather from './utils/getImageForWeather'
 
@@ -9,11 +9,14 @@ export default class App extends Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <ImageBackground source={getImageForWeather('Clear')} style={styles.imageContainer} imageStyle={styles.image} />
+        <ImageBackground source={getImageForWeather('Clear')} style={styles.imageContainer} imageStyle={styles.image}>
+          <View style={styles.detailsContainer}>
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
         <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     )
   }
@@ -22,7 +25,13 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#34495E'
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20
   },
   imageContainer: {
     flex: 1
