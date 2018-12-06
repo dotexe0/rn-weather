4abab97853302f635ed4fd75a9f9e4974c34c199
Daniel Varela <dvarela@wiser.com>
2018-12-06 09:20:56 -0800

    prettier

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
-import React from 'react';
-import { ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text } from 'react-native';
+import React, { Component } from 'react'
+import { ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text } from 'react-native'
 
 import getImageForWeather from './utils/getImageForWeather'
 
 import SearchInput from './components/SearchInput'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <ImageBackground
-          source={getImageForWeather('Clear')}
-          style={styles.imageContainer}
-          imageStyle={styles.image}
-        />
+        <ImageBackground source={getImageForWeather('Clear')} style={styles.imageContainer} imageStyle={styles.image} />
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-        <SearchInput placeholder="Search any city"/>
+        <SearchInput placeholder="Search any city" />
       </KeyboardAvoidingView>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
   },
   imageContainer: {
     flex: 1
@@ -54,4 +50,4 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 18
   }
-});
+})
