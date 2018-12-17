c5bb6d4339a77583a55b40dda7f9b18a59332de3
Daniel Varela <dotexe0@gmail.com>
2018-12-16 21:50:51 -0800

    update rules for db and clean index for ios

--- index.ios.js
+++ index.ios.js
@@ -18,15 +18,3 @@ var db = firebase.firestore();
 db.settings({
   timestampsInSnapshots: true
 });
-
-db.collection("users").add({
-  first: "Ada",
-  last: "Lovelace",
-  born: 1815
-})
-  .then(function (docRef) {
-    console.log("Document written with ID: ", docRef.id);
-  })
-  .catch(function (error) {
-    console.error("Error adding document: ", error);
-  });
